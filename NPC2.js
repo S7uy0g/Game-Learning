10878f4c53235fde526a1ec585700e00e62d7646
S7uy0g <RaiJoker103@gmail.com>
2023-10-22 12:34:16 +0545

    2ways of NPC movement

--- NPC2.js
+++ NPC2.js
@@ -6,32 +6,36 @@ CANVAS_HEIGHT=canvas.height=600;
 const numberOfEnemies=4;
 const enemyArray=[];
 const EnemyImage=new Image();
-EnemyImage.src='NPCRunAttack.png';
+EnemyImage.src='Ghost.png';
 let gameFrame=0;
-//Side to Side Movement
+//Side to Side with up and Down Movement
 class Enemy{
     constructor(EnemyImage){
-        this.x=Math.random()*canvas.width;
         //this.y=Math.random()*canvas.height;
         this.y=300;
         this.width=100;
         this.height=100;
+        this.x=Math.random()*(canvas.width-this.width);
         //this.speed=Math.random()*4-2;
         this.direction=1;
         this.EnemyImage=EnemyImage;
         this.enemyWidth=128;
         this.enemyHeight=128;
         this.frame=0;
-        this.bounceSpeed=Math.floor(Math.random()*3+3);
+        this.bounceSpeed=Math.floor(Math.random()*2+2);
+        this.angle=0;
+        this.angleSpeed=Math.random()*0.09+0.2;
     }
     update(){
-        this.x+=this.direction*Math.random()*this.bounceSpeed;
+        this.x+=this.direction*this.bounceSpeed;
+        this.y+=Math.sin(this.angle);
+        this.angle+=this.angleSpeed;
         //this.y+=this.speed;
-        if (this.x >= CANVAS_WIDTH || this.x <= 0) {
+        if (this.x >= CANVAS_WIDTH-100 || this.x <= 0) {
             this.direction = -this.direction; // Reverse the direction
         }
-        if(gameFrame%this.bounceSpeed===0){
-            this.frame > 8 ? this.frame= 0:this.frame++;
+        if(gameFrame%Math.floor(Math.random()*2+8)===0){
+            this.frame > 2 ? this.frame= 0:this.frame++;
         }
     }
     draw(){
